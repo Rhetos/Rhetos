fb4239c02fc09aeb0e6d63ea040ac3b37fb59518
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-02-26 09:33:27 +0100

    Improved XML cleanup.

--- Source/Rhetos.Persistence.NHibernate/NHibernateMappingGenerator.cs
+++ Source/Rhetos.Persistence.NHibernate/NHibernateMappingGenerator.cs
@@ -69,13 +69,18 @@ namespace Rhetos.Persistence.NHibernate
             return _mapping;
         }
 
+        const string detectLineTag = @"\n\s*<!--.*?-->\s*\r?\n";
+        const string detectTag = @"<!--.*?-->";
+
         private string GenerateMapping()
         {
             var sw = Stopwatch.StartNew();
 
             string innerXml = _codeGenerator.ExecutePlugins(_plugins, "<!--", "-->", null).GeneratedCode;
             innerXml = innerXml.Replace(AssemblyTag, _domainObjectModel.ObjectModel.FullName);
-            innerXml = Regex.Replace(innerXml, "<!--(.*)-->\\r?\\n?", string.Empty);
+            innerXml = Regex.Replace(innerXml, detectLineTag, "\n");
+            innerXml = Regex.Replace(innerXml, detectTag, "");
+            innerXml = innerXml.Trim();
 
             string xml =
 @"<?xml version=""1.0"" encoding=""utf-16"" ?>
