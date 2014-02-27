fd9a86672b64a9adebe3caeee02f134e61955d18
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-02-27 13:48:31 +0100

    Bugfix: Added dll dependencies for AspNetFormsAuth. These dlls are needed for SimpleMembershipProvider, but are not directly referenced from the application.

--- AspNetFormsAuth/CopyPlugins.bat
+++ AspNetFormsAuth/CopyPlugins.bat
@@ -11,4 +11,8 @@ XCOPY /Y/D/R %ThisScriptFolder%Plugins\AdminSetup\bin\%2\AdminSetup.exe.config %
 XCOPY /Y/D/R %ThisScriptFolder%Plugins\InitAspNetDatabase\bin\%2\InitAspNetDatabase.??? %1 || EXIT /B 1
 XCOPY /Y/D/R %ThisScriptFolder%Plugins\InitAspNetDatabase\bin\%2\InitAspNetDatabase.exe.config %1 || EXIT /B 1
 
+REM These dlls are required at runtime (by WebMatrix.WebData), but are not directly referenced in the application's code:
+XCOPY /Y/D/R %ThisScriptFolder%Plugins\AdminSetup\bin\%2\System.Web.WebPages.dll %1 || EXIT /B 1
+XCOPY /Y/D/R %ThisScriptFolder%Plugins\AdminSetup\bin\%2\System.Web.Helpers.dll %1 || EXIT /B 1
+
 EXIT /B 0
