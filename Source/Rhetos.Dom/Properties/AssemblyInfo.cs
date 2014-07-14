458bfd9916e9dbda8a7946ce8b1b1a553ad46bce
Bojan Antolovic <bojan.antolovic@gmail.com>
2014-07-14 16:26:17 +0200

    Build 0.9.17.2.

--- Source/Rhetos.Dom/Properties/AssemblyInfo.cs
+++ Source/Rhetos.Dom/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.17.1")]
-[assembly: AssemblyFileVersion("0.9.17.1")]
+[assembly: AssemblyVersion("0.9.17.3")]
+[assembly: AssemblyFileVersion("0.9.17.3")]
 
 [assembly: CLSCompliant(true)]
