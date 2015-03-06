00be25b135be3e21f923627a2b45e65d925eb116
Bojan Antolovic <bojan.antolovic@gmail.com>
2015-03-06 16:17:56 +0100

    Release 0.9.28.

--- Source/Rhetos/Properties/GlobalAssemblyInfo.cs
+++ Source/Rhetos/Properties/GlobalAssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) Omega software 2015")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("0.9.28")]
-[assembly: AssemblyFileVersion("0.9.28")]
-[assembly: AssemblyInformationalVersion("0.9.28-alpha001")]
+[assembly: AssemblyVersion("0.9.29")]
+[assembly: AssemblyFileVersion("0.9.29")]
+[assembly: AssemblyInformationalVersion("0.9.29-alpha001")]
