47f921b8420a5f8ec5999bb44cb0a6c35b8f8ea8
Bojan Antolovic <bojan.antolovic@gmail.com>
2015-08-17 15:06:11 +0200

    Build 1.0.0-alpha004.

--- Source/Rhetos/Properties/GlobalAssemblyInfo.cs
+++ Source/Rhetos/Properties/GlobalAssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha003")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha005")]
