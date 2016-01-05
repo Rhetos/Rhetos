5fc0a321936ff1c1287ab465ba1a4cc2eda52778
Bojan Antolovic <bojan.antolovic@gmail.com>
2016-01-05 11:36:09 +0100

    Clean.bat missed some folders. "dir TestResults" without wildcards listed only first subfolder.

--- Clean.bat
+++ Clean.bat
@@ -4,7 +4,7 @@ IF EXIST "Source\Rhetos\bin\ConnectionStrings.config" MOVE /Y "Source\Rhetos\bin
 REM Delete all "bin", "obj" and "TestResults" subfolders:
 @FOR /F "delims=" %%i IN ('dir bin /s/b/ad') DO DEL /F/S/Q "%%i" && RD /S/Q "%%i"
 @FOR /F "delims=" %%i IN ('dir obj /s/b/ad') DO DEL /F/S/Q "%%i" && RD /S/Q "%%i"
-@FOR /F "delims=" %%i IN ('dir TestResults /s/b/ad') DO DEL /F/S/Q "%%i" && RD /S/Q "%%i"
+@FOR /F "delims=" %%i IN ('dir TestResult? /s/b/ad') DO DEL /F/S/Q "%%i" && RD /S/Q "%%i"
 
 REM Delete packages' bineries and their copy in Rhetos folder:
 @DEL /F/Q "*.zip"
