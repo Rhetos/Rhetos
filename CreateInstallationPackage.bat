9c09c77c2e805b35b6402efebea32546b6eebf51
Bojan Antolović <bojan.antolovic@gmail.com>
2016-06-06 09:36:53 +0200

    Use 'lib' instead of 'plugins' subfolder in NuGet packages for Rhetos plugins (in progress) This allows simpler references to Rhetos dlls from other projects. Renamed Rhetos.Core NuGet to Rhetos.

--- CreateInstallationPackage.bat
+++ CreateInstallationPackage.bat
@@ -9,11 +9,11 @@ IF [%1] == [] SET Config=Debug
 
 CALL Source\Rhetos\GetServerFiles.bat %Config% /NOPAUSE || GOTO Error0
 
+.nuget\NuGet.exe pack Rhetos.nuspec -o Install || GOTO Error0
 .nuget\NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -o Install || GOTO Error0
 .nuget\NuGet.exe pack SimpleWindowsAuth\Rhetos.SimpleWindowsAuth.nuspec -o Install || GOTO Error0
 .nuget\NuGet.exe pack AspNetFormsAuth\Rhetos.AspNetFormsAuth.nuspec -o Install || GOTO Error0
 .nuget\NuGet.exe pack ActiveDirectorySync\Rhetos.ActiveDirectorySync.nuspec -o Install || GOTO Error0
-.nuget\NuGet.exe pack Rhetos.Core.nuspec -o Install || GOTO Error0
 
 MD Install\Rhetos
 MD Install\Rhetos\bin
