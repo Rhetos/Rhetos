e12b7bf86803a31e961a7f5c5ece828a7cdb67b3
Quoc Ngo <quoc.ngo@2clicksolutions.com>
2017-02-23 18:07:29 +0700

    Support password credential when creating database. * Refactored CreateAndSetDatabase CLI usages. * Added options param to CreateAndSetDatabase.exe to support password credential. * Added 1 more argument to SetupRhetosServer.bat. This argument is forwarded to CreateAndSetDatabase.exe.

---
+++ Source/CreateAndSetDatabase/CliOptionsParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CreateAndSetDatabase
+{
+    public static class CliOptionsParser
+    {
+        public static T Parse<T>(string[] arguments, bool hasCommand = false)
+            where T : CliOptions
+        {
+            var result = Activator.CreateInstance<T>();
+            var argumentQueue = new Queue<string>(arguments);
+
+            if (hasCommand)
+            {
+                result.Command = argumentQueue.Dequeue();
+            }
+
+            while(argumentQueue.Count > 0)
+            {
+                if (!argumentQueue.Peek().StartsWith("-"))
+                {
+                    break;
+                }
+
+                var option = argumentQueue.Dequeue();
+                var optionArgument = string.Empty;
+
+                if (result.RequireOptionArgument(option) && argumentQueue.Count > 0)
+                {
+                    if (!argumentQueue.Peek().StartsWith("-"))
+                    {
+                        optionArgument = argumentQueue.Dequeue();
+                    }
+                }
+
+                result.Options.Add(option.ToLower(), optionArgument);
+            }
+
+            result.Arguments = argumentQueue.ToArray();
+
+            result.Initialize();
+            return result;
+        }
+    }
+
+    public abstract class CliOptions
+    {
+        public CliOptions()
+        {
+            Options = new Dictionary<string, string>();
+        }
+
+        public string Command { get; set; }
+
+        public Dictionary<string, string> Options { get; set; }
+
+        public string[] Arguments { get; set; }
+
+        public virtual void Initialize() { }
+
+        public virtual bool RequireOptionArgument(string option)
+        {
+            return false;
+        }
+    }
+}
---
+++ Source/CreateAndSetDatabase/CreateAndSetDatabaseCliOptions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAndSetDatabase
+{
+    public class CreateAndSetDatabaseCliOptions : CliOptions
+    {
+        public bool Help { get; set; }
+
+        public string ServerName { get; set; }
+
+        public bool UseSSPI { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Password { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public override void Initialize()
+        {
+            Help = this.Options.ContainsKey("-h");
+
+            if (Help)
+            {
+                return;
+            }
+
+            if (this.Arguments.Length < 2)
+            {
+                throw new ArgumentException("SQLServer must be specified.");
+            }
+
+            ServerName = this.Arguments[0];
+            DatabaseName = this.Arguments[1];
+
+            UseSSPI = !this.Options.ContainsKey("-c");
+            if (!UseSSPI)
+            {
+                if (!this.Options.ContainsKey("-userid"))
+                {
+                    Console.Write("Database user name: ");
+                    UserId = Console.ReadLine();
+                }
+                else
+                {
+                    UserId = this.Options["-userid"];
+                }
+
+                if (!this.Options.ContainsKey("-pw"))
+                {
+                    Console.Write("Database password: ");
+                    Password = Console.ReadLine();
+                }
+                else
+                {
+                    Password = this.Options["-pw"];
+                }
+            }
+        }
+
+        public override bool RequireOptionArgument(string option)
+        {
+            var argumentRequiredOptions = new[] { "-userid", "-pw" };
+
+            return argumentRequiredOptions.Contains(option);
+        }
+
+        public static void ShowHelp()
+        {
+            Console.WriteLine(
+@"Usage: CreateAndSetDatabase [options] <SQLServer> <DataBaseName>
+    options:
+        -h                  show help
+        -c                  use datbase login credential instead of integrated security, if specified, userId and password will be asked
+        -userid <UserId>    database's user ID
+        -pw <Password>      database's password
+"
+            );
+        }
+    }
+}
