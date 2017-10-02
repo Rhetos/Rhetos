52ea96bcabe9a94f878d66dae126d2e5ce5b0b1e
Bojan Antolović <bojan.antolovic@gmail.com>
2017-10-02 22:37:10 +0200

    Development 2.1.0.

--- Source/Rhetos/Properties/GlobalAssemblyInfo.cs
+++ Source/Rhetos/Properties/GlobalAssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) Omega software 2017")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-dev")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.0-dev")]
