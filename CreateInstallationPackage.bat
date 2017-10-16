72ad554179b56bd66b098ebe3ed4c73d1e768852
Bojan Antolović <bojan.antolovic@gmail.com>
2017-10-16 23:59:49 +0200

    AspNetFormsAuth package is moved to a separate source repository: https://github.com/Rhetos/AspNetFormsAuth

--- CreateInstallationPackage.bat
+++ CreateInstallationPackage.bat
@@ -11,7 +11,6 @@ CALL Source\Rhetos\GetServerFiles.bat %Config% /NOPAUSE || GOTO Error0
 
 NuGet.exe pack Rhetos.nuspec -o Install || GOTO Error0
 NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -o Install || GOTO Error0
-NuGet.exe pack AspNetFormsAuth\Rhetos.AspNetFormsAuth.nuspec -o Install || GOTO Error0
 NuGet.exe pack ActiveDirectorySync\Rhetos.ActiveDirectorySync.nuspec -o Install || GOTO Error0
 
 MD Install\Rhetos
