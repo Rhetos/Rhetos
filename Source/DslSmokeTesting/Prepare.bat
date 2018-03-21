e8718a850e09d0dd933fc00f2b2ec90cfc252beb
Bojan Antolović <bojan.antolovic@gmail.com>
2018-03-21 16:34:03 +0100

    Removed obsolete smoke test.

--- Source/DslSmokeTesting/Prepare.bat
+++
@@ -1,8 +0,0 @@
-IF EXIST bin RD bin /S /Q
-REM Allow Windows Explorer to release file locks.
-IF EXIST bin RD bin /S /Q
-IF EXIST bin RD bin /S /Q
-IF EXIST bin ECHO ERROR: Cannot clear LOCKED folder 'bin' & PAUSE
-MD bin
-
-XCOPY /S/Y/D/R ..\DeployPackages\bin\* bin\
