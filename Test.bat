cca1bf72cafa478d45b1cdbe50b586f389401990
Bojan Antolović <bojan.antolovic@gmail.com>
2018-06-14 15:16:22 +0200

    Removed obsolete test files.

--- Test.bat
+++ Test.bat
@@ -5,27 +5,8 @@
 
 CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 
-@IF EXIST Test.log DEL Test.log
-@DATE /T >> Test.log
-@TIME /T >> Test.log
-
-IF NOT EXIST TestResults MD TestResults
-
-@REM For each test project CALL:TEST
-DIR "*.Test.dll" /s/b | FINDSTR /I "\\bin\\%Config%\\" | FINDSTR /I /V "\\TestResults\\" > AllTestProjects.txt
-FOR /F "delims=;" %%P IN ('TYPE AllTestProjects.txt') DO @CALL:TEST "%%P"
-DEL AllTestProjects.txt
-
-@ECHO. >> Test.log
-@DATE /T >> Test.log
-@TIME /T >> Test.log
-
-@REM Error analysis:
-FINDSTR /N /I /R "\<error\> \<errors\> \<fail\> \<failed\> \<skipped\> \<not.found\>" Test.log | FINDSTR /I /R /V "\<0.error \<0.fail TestCaseManagement.QualityToolsPackage error.ico Warning:" > TestErrors.log
-
-FOR /F "usebackq" %%A IN ('TestErrors.log') DO SET Size=%%~zA
-IF 0%Size% GTR 0 TYPE TestErrors.log && GOTO Error0
-@SET Size=
+@REM Find all test projects, and execute tests for each one:
+FOR /F "tokens=*" %%a IN ('DIR "*.Test.dll" /s/b ^| FINDSTR /I ^"\\bin\\%Config%\\^"') DO vstest.console.exe "%%a" || GOTO Error0
 
 @REM ================================================
 
@@ -38,12 +19,3 @@ IF 0%Size% GTR 0 TYPE TestErrors.log && GOTO Error0
 @ECHO %~nx0 FAILED.
 @IF /I [%2] NEQ [/NOPAUSE] @PAUSE
 @EXIT /B 1
-
-:TEST
-
-@ECHO TESTING %~nx1:
-@ECHO. >> Test.log
-@ECHO == %1 == >> Test.log
-MSTest /testcontainer:%1 /runconfig:"%~dp0Local.testsettings" >> Test.log
-@REM Wait a second to make sure the .trx file with the next test results has a unique name (filename contains current time)
-@TIMEOUT /T 1 /NOBREAK 1> NUL 2> NUL
