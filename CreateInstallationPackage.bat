701595ef276612587a58f1e760691de60404b557
Bojan Antolović <bojan.antolovic@gmail.com>
2018-09-28 12:01:52 +0200

    Renaming Rhetos server installation package form Rhetos.x.y.z.zip to RhetosServer.x.y.z.zip.

--- CreateInstallationPackage.bat
+++ CreateInstallationPackage.bat
@@ -1,8 +1,8 @@
 IF NOT EXIST Install\ MD Install
 DEL /F /S /Q Install\* || GOTO Error0
 IF EXIST Install\*.zip ECHO ERROR: Cannot delete old files in Install folder. Check if the files are locked. && GOTO Error0
-RD /S /Q Install\Rhetos
-IF EXIST Install\Rhetos ECHO ERROR: Cannot delete old files in Install folder. Check if the files are locked. && GOTO Error0
+RD /S /Q Install\RhetosServer
+IF EXIST Install\RhetosServer ECHO ERROR: Cannot delete old files in Install folder. Check if the files are locked. && GOTO Error0
 
 SET Config=%1%
 IF [%1] == [] SET Config=Debug
@@ -15,25 +15,25 @@ IF NOT EXIST Install\nuget.exe POWERSHELL (New-Object System.Net.WebClient).Down
 Install\NuGet.exe pack Rhetos.nuspec -OutputDirectory Install || GOTO Error0
 Install\NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -OutputDirectory Install || GOTO Error0
 
-MD Install\Rhetos
-MD Install\Rhetos\bin
+MD Install\RhetosServer
+MD Install\RhetosServer\bin
 
-XCOPY /Y/D/R Source\Rhetos\bin\*.dll Install\Rhetos\bin || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\bin\*.pdb Install\Rhetos\bin || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\bin\*.exe Install\Rhetos\bin || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\bin\*.config Install\Rhetos\bin || GOTO Error0
-DEL /F /Q Install\Rhetos\bin\ConnectionStrings.config
+XCOPY /Y/D/R Source\Rhetos\bin\*.dll Install\RhetosServer\bin || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\bin\*.pdb Install\RhetosServer\bin || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\bin\*.exe Install\RhetosServer\bin || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\bin\*.config Install\RhetosServer\bin || GOTO Error0
+DEL /F /Q Install\RhetosServer\bin\ConnectionStrings.config
 
-XCOPY /Y/D/R Source\Rhetos\*.aspx Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\*.asax Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\Web.config Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\Template.RhetosPackages.config Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\Template.RhetosPackageSources.config Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\*.linq Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Source\Rhetos\*.svc Install\Rhetos\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\*.aspx Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\*.asax Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\Web.config Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\Template.RhetosPackages.config Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\Template.RhetosPackageSources.config Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\*.linq Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Source\Rhetos\*.svc Install\RhetosServer\ || GOTO Error0
 
-XCOPY /Y/D/R ChangeLog.md Install\Rhetos\ || GOTO Error0
-XCOPY /Y/D/R Readme.md Install\Rhetos\ || GOTO Error0
+XCOPY /Y/D/R ChangeLog.md Install\RhetosServer\ || GOTO Error0
+XCOPY /Y/D/R Readme.md Install\RhetosServer\ || GOTO Error0
 
 @REM ================================================
 
