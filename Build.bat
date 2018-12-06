60104fb12ebc40fa03a01abba9b2ae36d99226f6
Bojan Antolović <bojan.antolovic@gmail.com>
2018-12-06 15:00:35 +0100

    Development 2.11.0.

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 SETLOCAL
-SET Version=2.10.0
+SET Version=2.11.0
 SET Prerelease=auto
 
 @SET Config=%1%
--- Source/Rhetos/Properties/GlobalAssemblyInfo.cs
+++ Source/Rhetos/Properties/GlobalAssemblyInfo.cs
@@ -26,6 +26,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright (C) Omega software 2017")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("2.10.0")]
-[assembly: AssemblyFileVersion("2.10.0")]
-[assembly: AssemblyInformationalVersion("2.10.0-dev")]
+[assembly: AssemblyVersion("2.11.0")]
+[assembly: AssemblyFileVersion("2.11.0")]
+[assembly: AssemblyInformationalVersion("2.11.0-dev")]
