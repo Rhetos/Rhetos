edebee2cac71c2477084d1e2582e0ec219e41e87
Sasa Stublic <sasa.stublic@gmail.com>
2019-09-16 18:39:34 +0200

    Add DeployPackages test project.

---
+++ Source/DeployPackages.Test/AutofacConfigurationTest.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhetos.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeployPackages.Test
+{
+    [TestClass]
+    public class AutofacConfigurationTest
+    {
+        [TestMethod]
+        public void ContainerContainsAllComponents()
+        {
+            Paths.InitializeRhetosServer();
+
+            var builder = new ContainerBuilder();
+            var arguments = new DeployArguments(new string[] { } ); // "/DatabaseOnly"
+            builder.RegisterModule(new AutofacModuleConfiguration(deploymentTime: true, arguments));
+
+            using (var container = builder.Build())
+            {
+                var regs = container.ComponentRegistry.Registrations
+                    .Select(a => a.ToString())
+                    .OrderBy(a => a)
+                    .ToList();
+                foreach (var r in regs)
+                {
+                    System.Diagnostics.Trace.WriteLine(r);
+                }
+            }
+        }
+    }
+}
