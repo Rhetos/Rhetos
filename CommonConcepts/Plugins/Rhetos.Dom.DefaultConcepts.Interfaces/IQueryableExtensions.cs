815db9d4e3c83e6745e3d580cb4d17909a3b253b
Ivan Barbančić <ibarbancic@omega-software.hr>
2019-10-23 10:24:05 +0200

    Entity framework query optimizations

---
+++ CommonConcepts/Plugins/Rhetos.Dom.DefaultConcepts.Interfaces/IQueryableExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Rhetos.Dom.DefaultConcepts
+{
+    public static class IQueryableExtensions
+    {
+        public static IQueryable<T> WhereContains<T>(this IQueryable<T> query, List<Guid> ids, Expression<Func<T, Guid>> predicate)
+        {
+            Expression<Func<List<Guid>>> idsLambda = () => ids;
+            ParameterExpression parameter = Expression.Parameter(typeof(T), "p");
+            var predicateReplacement = (Expression<Func<T, Guid>>)(new ReplaceParameterVisitor(predicate.Parameters.Single(), parameter)).Visit(predicate);
+            var finalExpression = (Expression<Func<T, bool>>)Expression.Lambda(
+                Expression.Call(
+                    idsLambda.Body,
+                    typeof(List<Guid>).GetMethod(
+                        "Contains",
+                        BindingFlags.Public | BindingFlags.Instance,
+                        null,
+                        CallingConventions.Any,
+                        new Type[] { typeof(Guid) },
+                        null
+                    ),
+                predicateReplacement.Body), parameter);
+            return query.Where(EFExpression.OptimizeContains(finalExpression));
+        }
+
+        private class ReplaceParameterVisitor : ExpressionVisitor
+        {
+            Expression _left;
+            Expression _right;
+
+            public ReplaceParameterVisitor(Expression left, Expression right)
+            {
+                _left = left;
+                _right = right;
+            }
+
+            public override Expression Visit(Expression node)
+            {
+                if (node.Equals(_left))
+                {
+                    return _right;
+                }
+
+                return base.Visit(node);
+            }
+        }
+    }
+}
