6828df695b1fe6c5882744495fe821da6b226410
Bojan Antolović <bojan.antolovic@gmail.com>
2020-01-22 18:19:08 +0100

    Separated build and runtime configuration in DeployPackages.

--- Source/DeployPackages/Program.cs
+++ Source/DeployPackages/Program.cs
@@ -53,31 +53,60 @@ namespace DeployPackages
                 if (!ValidateArguments(args))
                     return 1;
 
-                var configurationProvider = BuildConfigurationProvider(args);
-                var deployOptions = configurationProvider.GetOptions<DeployOptions>();
-
-                pauseOnError = !deployOptions.NoPause;
-
-                if (deployOptions.StartPaused)
-                    StartPaused();
-
-                var deployment = new ApplicationDeployment(configurationProvider, logProvider, LegacyUtilities.GetListAssembliesDelegate());
-                if (!deployOptions.DatabaseOnly)
+                string rhetosAppRootPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".."));
+                
+                // Using build-time configuration:
                 {
-                    DeleteObsoleteFiles(logProvider, logger);
-                    var installedPackages = deployment.DownloadPackages(deployOptions.IgnoreDependencies);
-                    deployment.GenerateApplication(installedPackages);
-                }
-                else
-                {
-                    logger.Info("Skipped deleting old generated files (DeployDatabaseOnly).");
-                    logger.Info("Skipped download packages (DeployDatabaseOnly).");
-                    logger.Info("Skipped code generators (DeployDatabaseOnly).");
+                    var configurationProvider = new ConfigurationBuilder()
+                        .AddRhetosAppEnvironment(new RhetosAppEnvironment
+                        {
+                            RootPath = rhetosAppRootPath,
+                            BinFolder = Path.Combine(rhetosAppRootPath, @"bin"),
+                            AssetsFolder = Path.Combine(rhetosAppRootPath, @"bin\Generated"),
+                        })
+                        .AddKeyValue(nameof(BuildOptions.CacheFolder), Path.Combine(rhetosAppRootPath, "GeneratedFilesCache"))
+                        .AddKeyValue(nameof(BuildOptions.GeneratedSourceFolder), Path.Combine(rhetosAppRootPath, @"bin\Generated"))
+                        .AddWebConfiguration(rhetosAppRootPath)
+                        .AddConfigurationManagerConfiguration()
+                        .AddCommandLineArguments(args, "/")
+                        .Build();
+
+                    var deployOptions = configurationProvider.GetOptions<DeployOptions>();
+
+                    pauseOnError = !deployOptions.NoPause;
+
+                    if (deployOptions.StartPaused)
+                        StartPaused();
+
+                    var deployment = new ApplicationDeployment(configurationProvider, logProvider, LegacyUtilities.GetListAssembliesDelegate());
+                    if (!deployOptions.DatabaseOnly)
+                    {
+                        DeleteObsoleteFiles(logProvider, logger);
+                        var installedPackages = deployment.DownloadPackages(deployOptions.IgnoreDependencies);
+                        deployment.GenerateApplication(installedPackages);
+                    }
+                    else
+                    {
+                        logger.Info("Skipped deleting old generated files (DeployDatabaseOnly).");
+                        logger.Info("Skipped download packages (DeployDatabaseOnly).");
+                        logger.Info("Skipped code generators (DeployDatabaseOnly).");
+                    }
                 }
 
-                deployment.UpdateDatabase();
-                deployment.InitializeGeneratedApplication();
-                deployment.RestartWebServer();
+                // Using run-time configuration:
+                {
+                    var configurationProvider = new ConfigurationBuilder()
+                        .AddRhetosAppConfiguration(rhetosAppRootPath)
+                        .AddConfigurationManagerConfiguration()
+                        .AddCommandLineArguments(args, "/")
+                        .Build();
+
+                    var deployment = new ApplicationDeployment(configurationProvider, logProvider, LegacyUtilities.GetListAssembliesDelegate());
+
+                    deployment.UpdateDatabase();
+                    deployment.InitializeGeneratedApplication();
+                    deployment.RestartWebServer();
+                }
 
                 logger.Trace("Done.");
             }
@@ -96,19 +125,6 @@ namespace DeployPackages
             }
 
             return 0;
-        }
-
-        private static IConfigurationProvider BuildConfigurationProvider(string[] args)
-        {
-            string rhetosAppRootPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".."));
-
-            return new ConfigurationBuilder()
-                .AddKeyValue(nameof(BuildOptions.CacheFolder), Path.Combine(rhetosAppRootPath, "GeneratedFilesCache"))
-                .AddKeyValue(nameof(BuildOptions.GeneratedSourceFolder), Path.Combine(rhetosAppRootPath, "bin\\Generated"))
-                .AddRhetosAppConfiguration(rhetosAppRootPath)
-                .AddConfigurationManagerConfiguration()
-                .AddCommandLineArguments(args, "/")
-                .Build();
         }
 
         /// <summary>
