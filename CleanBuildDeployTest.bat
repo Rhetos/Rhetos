a7f343dbbf3f417eeced5665250a716781503a7f
Bojan Antolović <bojan.antolovic@gmail.com>
2020-04-11 22:09:15 +0200

    Missing NuGet restore for CommonConceptsTest. It did not use NuGet in previous versions.

--- CleanBuildDeployTest.bat
+++ CleanBuildDeployTest.bat
@@ -10,6 +10,7 @@ CALL Build.bat %Config% /NOPAUSE || GOTO Error0
 REM Deploying test application for integration tests:
 Source\Rhetos\bin\DeployPackages.exe /NoPause /Debug /ShortTransactions || GOTO Error0
 CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
+NuGet restore CommonConceptsTest.sln
 MSBuild.exe CommonConceptsTest.sln /target:rebuild /p:Configuration=Debug /verbosity:minimal || GOTO Error0
 
 CALL Test.bat %Config% /NOPAUSE || GOTO Error0
