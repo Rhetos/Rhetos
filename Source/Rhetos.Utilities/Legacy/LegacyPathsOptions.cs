ace56360edd380d6011df6d767fa8d50fdd2b834
Bojan Antolović <bojan.antolovic@gmail.com>
2020-04-23 22:55:15 +0200

    Simplified LegacyPathsOptions configuration path.

--- Source/Rhetos.Utilities/Legacy/LegacyPathsOptions.cs
+++ Source/Rhetos.Utilities/Legacy/LegacyPathsOptions.cs
@@ -23,7 +23,7 @@ namespace Rhetos.Utilities
     /// Legacy paths, supporting build process with DeployPackages and obsolete <see cref="Paths"/> class.
     /// Available at both build-time and run-time.
     /// </summary>
-    [Options("Legacy:Paths")]
+    [Options("LegacyPaths")]
     public class LegacyPathsOptions
     {
         /// <summary>
