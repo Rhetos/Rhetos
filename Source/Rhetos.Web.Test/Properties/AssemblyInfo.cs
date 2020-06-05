b11f9d83d75b9bbe76706231750e4b9bb45e968f
Bojan Antolović <bojan.antolovic@gmail.com>
2020-06-05 13:35:39 +0200

    Development 4.0.1.

--- Source/Rhetos.Web.Test/Properties/AssemblyInfo.cs
+++ Source/Rhetos.Web.Test/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("31170410-b3e6-47df-a651-2d2d4a25c676")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyFileVersion("4.0.1")]
