d103f491d6db4bc81e0fe406a1e77c3b3fb65933
Bojan Antolović <bojan.antolovic@gmail.com>
2020-06-16 09:20:58 +0200

    Support for build with VS2015, VS2017 and VS2019.

--- Build.bat
+++ Build.bat
@@ -3,16 +3,7 @@
 @SET Config=%1%
 @IF [%1] == [] SET Config=Debug
 
-@IF DEFINED VisualStudioVersion GOTO SkipVcvarsall
-IF "%VS140COMNTOOLS%" NEQ "" CALL "%VS140COMNTOOLS%VsDevCmd.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS120COMNTOOLS%" NEQ "" CALL "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS110COMNTOOLS%" NEQ "" CALL "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS100COMNTOOLS%" NEQ "" CALL "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-ECHO ERROR: Cannot detect Visual Studio, missing VSxxxCOMNTOOLS variable.
-GOTO Error0
-:EndVcvarsall
-@ECHO ON
-:SkipVcvarsall
+CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 
 IF EXIST Build.log DEL Build.log || GOTO Error0
 DevEnv.com "Rhetos.sln" /rebuild %Config% /out Build.log || TYPE Build.log && GOTO Error0
---
+++ Tools/Build/FindVisualStudio.bat
@@ -0,0 +1,24 @@
+@REM Starts Visual Studio Developer Command Prompt.
+@IF DEFINED VisualStudioVersion EXIT /B 0
+
+@REM https://developercommunity.visualstudio.com/content/problem/26780/vsdevcmdbat-changes-the-current-working-directory.html
+SET "VSCMD_START_DIR=%CD%"
+
+@REM VS2017 uses vswhere.exe for locating the installation.
+FOR /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
+  IF EXIST "%%i\Common7\Tools\VsDevCmd.bat" (CALL "%%i\Common7\Tools\VsDevCmd.bat" && GOTO OkRestoreEcho || GOTO Error)
+)
+
+@REM VS2015 uses %VS140COMNTOOLS% for locating the installation.
+IF "%VS140COMNTOOLS%" NEQ "" (
+  IF EXIST "%VS140COMNTOOLS%VsDevCmd.bat" (
+    CALL "%VS140COMNTOOLS%VsDevCmd.bat" x86 && GOTO OkRestoreEcho || GOTO Error
+  )
+)
+
+:Error
+@ECHO ERROR: Cannot find Visual Studio.
+@EXIT /B 1
+:OkRestoreEcho
+@ECHO ON
+@ECHO %~nx0 SUCCESSFULLY COMPLETED.
