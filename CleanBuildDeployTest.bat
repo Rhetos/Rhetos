60d1e4525e2b7f6e42198784c973d7a80f83015f
Ivan Barbančić <ibarbancic@omega-software.hr>
2020-11-09 15:03:15 +0100

    Using multitargeting for RhetosVSIntegration project because it needs to support both .NET Core and .NET Frameowrk

--- CleanBuildDeployTest.bat
+++ CleanBuildDeployTest.bat
@@ -7,8 +7,6 @@ CALL Clean.bat || GOTO Error0
 
 CALL Build.bat %Config% /NOPAUSE || GOTO Error0
 
-CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
-
 CALL Test.bat %Config% /NOPAUSE || GOTO Error0
 
 @REM ================================================
--- Test.bat
+++ Test.bat
@@ -3,19 +3,22 @@
 @SET Config=%1%
 @IF [%1] == [] SET Config=Debug
 
-CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
-
 @REM Find all test projects, and execute tests for each one:
 @REM The platform switch is passed so that the test project behaves the same as in Visual Studio
-vstest.console.exe "Source\Rhetos.Configuration.Autofac.Test\bin\Debug\netcoreapp3.1\Rhetos.Configuration.Autofac.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.DatabaseGenerator.Test\bin\Debug\netcoreapp3.1\Rhetos.DatabaseGenerator.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Dsl.Test\bin\Debug\netcoreapp3.1\Rhetos.Dsl.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Extensibility.Test\bin\Debug\netcoreapp3.1\Rhetos.Extensibility.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Logging.Test\bin\Debug\netcoreapp3.1\Rhetos.Logging.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Persistence.Test\bin\Debug\netcoreapp3.1\Rhetos.Persistence.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Utilities.Test\bin\Debug\netcoreapp3.1\Rhetos.Utilities.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "Source\Rhetos.Deployment.Test\bin\Debug\netcoreapp3.1\Rhetos.Deployment.Test.dll" /Platform:x86 || GOTO Error0
-vstest.console.exe "CommonConcepts\CommonConcepts.Test\bin\Debug\netcoreapp3.1\CommonConcepts.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Utilities.Test\bin\Debug\netcoreapp3.1\Rhetos.Utilities.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Persistence.Test\bin\Debug\netcoreapp3.1\Rhetos.Persistence.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Configuration.Autofac.Test\bin\Debug\netcoreapp3.1\Rhetos.Configuration.Autofac.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.DatabaseGenerator.Test\bin\Debug\netcoreapp3.1\Rhetos.DatabaseGenerator.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Dsl.Test\bin\Debug\netcoreapp3.1\Rhetos.Dsl.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Extensibility.Test\bin\Debug\netcoreapp3.1\Rhetos.Extensibility.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Logging.Test\bin\Debug\netcoreapp3.1\Rhetos.Logging.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Deployment.Test\bin\Debug\netcoreapp3.1\Rhetos.Deployment.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "CommonConcepts\CommonConcepts.Test\bin\Debug\netcoreapp3.1\CommonConcepts.Test.dll" /Platform:x86 || GOTO Error0
+
+CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
+@REM We are tsestin if the CommonConceptsTest.sln could be restored with the MSBuild command
+@REM because the target frameworks used to run custom MSBuild tasks are different when using dotnet cli and MSBuild.exe
+MSBuild.exe CommonConceptsTest.sln /target:restore /p:RestoreForce=True  /target:rebuild /p:Configuration=Debug /verbosity:minimal || GOTO Error0
 
 @REM ================================================
 
