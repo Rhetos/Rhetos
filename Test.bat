96798ad32a415f05a6aaa1eff6ae9f640443ae59
Ivan Barbančić <ibarbancic@omega-software.hr>
2020-11-30 10:09:15 +0100

    Using x64 arhitecture when runngin tests

--- Test.bat
+++ Test.bat
@@ -5,18 +5,18 @@ SETLOCAL
 
 @REM Find all test projects, and execute tests for each one:
 @REM The platform switch is passed so that the test project behaves the same as in Visual Studio
-dotnet test "Source\Rhetos.Utilities.Test\bin\Debug\net5.0\Rhetos.Utilities.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Persistence.Test\bin\Debug\net5.0\Rhetos.Persistence.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Configuration.Autofac.Test\bin\Debug\net5.0\Rhetos.Configuration.Autofac.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.DatabaseGenerator.Test\bin\Debug\net5.0\Rhetos.DatabaseGenerator.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Dsl.Test\bin\Debug\net5.0\Rhetos.Dsl.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Extensibility.Test\bin\Debug\net5.0\Rhetos.Extensibility.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Logging.Test\bin\Debug\net5.0\Rhetos.Logging.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "Source\Rhetos.Deployment.Test\bin\Debug\net5.0\Rhetos.Deployment.Test.dll" /Platform:x86 || GOTO Error0
-dotnet test "CommonConcepts\Plugins\Rhetos.CommonConcepts.Test\bin\Debug\net5.0\Rhetos.CommonConcepts.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "Source\Rhetos.Utilities.Test\bin\Debug\net5.0\Rhetos.Utilities.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Persistence.Test\bin\Debug\net5.0\Rhetos.Persistence.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Configuration.Autofac.Test\bin\Debug\net5.0\Rhetos.Configuration.Autofac.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.DatabaseGenerator.Test\bin\Debug\net5.0\Rhetos.DatabaseGenerator.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Dsl.Test\bin\Debug\net5.0\Rhetos.Dsl.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Extensibility.Test\bin\Debug\net5.0\Rhetos.Extensibility.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Logging.Test\bin\Debug\net5.0\Rhetos.Logging.Test.dll" || GOTO Error0
+dotnet test "Source\Rhetos.Deployment.Test\bin\Debug\net5.0\Rhetos.Deployment.Test.dll" || GOTO Error0
+dotnet test "CommonConcepts\Plugins\Rhetos.CommonConcepts.Test\bin\Debug\net5.0\Rhetos.CommonConcepts.Test.dll" || GOTO Error0
 
 dotnet build CommonConceptsTest.sln /target:restore /p:RestoreForce=True /target:rebuild /p:Configuration=Debug /verbosity:minimal || GOTO Error0
-dotnet test "CommonConcepts\CommonConcepts.Test\bin\Debug\net5.0\CommonConcepts.Test.dll" /Platform:x86 || GOTO Error0
+dotnet test "CommonConcepts\CommonConcepts.Test\bin\Debug\net5.0\CommonConcepts.Test.dll" || GOTO Error0
 
 CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 @REM Using MSBuild to test if the CommonConceptsTest.sln could be restored with the MSBuild command
