72b29bf39a9af2d44b09a2154ef8d58027459af8
Bojan Antolović <bojan.antolovic@gmail.com>
2021-01-30 20:50:41 +0100

    Undoing registration of IPersistenceTransaction on DatabaseUpgrade. It is intended to run without a global transaction, to allow a better control over the transaction (for example, DbUpdateOptions.ShortTransactions and SqlUtility.NoTransactionTag).

--- Source/Rhetos.Configuration.Autofac/Modules/DatabaseRuntimeModule.cs
+++ Source/Rhetos.Configuration.Autofac/Modules/DatabaseRuntimeModule.cs
@@ -31,7 +31,6 @@ namespace Rhetos.Configuration.Autofac.Modules
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterInstance(new ConnectionString(SqlUtility.ConnectionString));
-            builder.RegisterType<PersistenceTransaction>().As<IPersistenceTransaction>().InstancePerLifetimeScope();
             builder.RegisterType(DatabaseTypes.GetSqlExecuterType(SqlUtility.DatabaseLanguage)).As<ISqlExecuter>().InstancePerLifetimeScope();
             builder.Register(context => context.Resolve<IConfiguration>().GetOptions<SqlTransactionBatchesOptions>()).InstancePerLifetimeScope();
             builder.RegisterType<SqlTransactionBatches>().InstancePerLifetimeScope();
