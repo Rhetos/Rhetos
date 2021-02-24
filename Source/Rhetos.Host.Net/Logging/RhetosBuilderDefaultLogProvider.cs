a1e5f26ce9469f6da62a8b68265f21ded43012a6
Bojan Antolović <bojan.antolovic@gmail.com>
2021-02-24 15:59:36 +0100

    Updated namespaces and license headers.

--- Source/Rhetos.Host.Net/Logging/RhetosBuilderDefaultLogProvider.cs
+++ Source/Rhetos.Host.Net/Logging/RhetosBuilderDefaultLogProvider.cs
@@ -1,33 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
-using Microsoft.Extensions.Logging;
-using Rhetos.Logging;
-using ILogger = Rhetos.Logging.ILogger;
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
 
-namespace Rhetos.Extensions.NetCore.Logging
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.Extensions.Logging;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Rhetos.Host.Net.Logging
 {
     // Leverage .NET Core logging infrastructure to create default cross-platform logger which logs to three different targets:
     // Console, Debug and EventLog(Windows only)
-    public sealed class RhetosBuilderDefaultLogProvider : ILogProvider, IDisposable
+    public sealed class RhetosBuilderDefaultLogProvider : Rhetos.Logging.ILogProvider, IDisposable
     {
         private readonly Lazy<ILoggerFactory> loggerFactory = new Lazy<ILoggerFactory>(CreateLoggerFactory);
-        public ILogger GetLogger(string name)
+
+        public Rhetos.Logging.ILogger GetLogger(string eventName)
         {
-            return new RhetosNetCoreLogger(loggerFactory.Value.CreateLogger(name), name);
+            return new RhetosNetCoreLogger(loggerFactory.Value.CreateLogger(eventName), eventName);
         }
 
         private static ILoggerFactory CreateLoggerFactory()
         {
-            var loggerFactory = LoggerFactory.Create(builder =>
+            return LoggerFactory.Create(builder =>
             {
                 builder.AddConsole();
                 builder.AddDebug();
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     builder.AddEventLog();
             });
-            return loggerFactory;
         }
 
         public void Dispose()
--- Source/Rhetos.Host.Net/Logging/RhetosNetCoreLogger.cs
+++ Source/Rhetos.Host.Net/Logging/RhetosNetCoreLogger.cs
@@ -1,35 +1,53 @@
-﻿using System;
-using Microsoft.Extensions.Logging;
-using Rhetos.Logging;
-using ILogger = Rhetos.Logging.ILogger;
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
 
-namespace Rhetos.Extensions.NetCore.Logging
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Rhetos.Host.Net.Logging
 {
-    public class RhetosNetCoreLogger : ILogger
+    public class RhetosNetCoreLogger : Rhetos.Logging.ILogger
     {
         public string Name { get; }
 
         private readonly Microsoft.Extensions.Logging.ILogger netCoreLogger;
+
         public RhetosNetCoreLogger(Microsoft.Extensions.Logging.ILogger netCoreLogger, string name)
         {
             this.netCoreLogger = netCoreLogger;
             this.Name = name;
         }
 
-        public void Write(EventType eventType, Func<string> logMessage)
+        public void Write(Rhetos.Logging.EventType eventType, Func<string> logMessage)
         {
             switch (eventType)
             {
-                case EventType.Trace:
+                case Rhetos.Logging.EventType.Trace:
                     netCoreLogger.LogTrace(logMessage());
                     break;
-                case EventType.Info:
+                case Rhetos.Logging.EventType.Info:
                     netCoreLogger.LogInformation(logMessage());
                     break;
-                case EventType.Warning:
+                case Rhetos.Logging.EventType.Warning:
                     netCoreLogger.LogWarning(logMessage());
                     break;
-                case EventType.Error:
+                case Rhetos.Logging.EventType.Error:
                     netCoreLogger.LogError(logMessage());
                     break;
             }
