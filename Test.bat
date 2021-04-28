b9b3077959fd9e6bc97b6929aec3b2e59ccc9e47
Bojan Antolović <bojan.antolovic@gmail.com>
2021-04-28 12:38:00 +0200

    Added custom assembly resolver in ResolveRhetosProjectAssets to fix the issue with dependency version: The "ResolveRhetosProjectAssets" task failed unexpectedly. System.IO.FileNotFoundException: Could not load file or assembly 'Newtonsoft.Json, Version=9.0.0.0, ... This closes #432.

--- Test.bat
+++ Test.bat
@@ -9,6 +9,7 @@ dotnet test Rhetos.sln --no-build || GOTO Error0
 @REM Using "RestoreForce" to make sure that the new version of local Rhetos NuGet packages are included in build.
 IF NOT EXIST CommonConcepts\CommonConcepts.Test\rhetos-app.local.settings.json COPY Tools\Configuration\Template.rhetos-app.local.settings.json CommonConcepts\CommonConcepts.Test\rhetos-app.local.settings.json
 dotnet build CommonConceptsTest.sln /t:restore /p:RestoreForce=True /t:rebuild --configuration %Config% || GOTO Error0
+@REM Running dbupdate again to test the CLI (it was executed in the build above).
 CommonConcepts\CommonConcepts.Test\bin\Debug\net5.0\rhetos.exe dbupdate CommonConcepts\CommonConcepts.Test\bin\Debug\net5.0\CommonConcepts.Test.dll
 dotnet test CommonConceptsTest.sln --no-build || GOTO Error0
 
