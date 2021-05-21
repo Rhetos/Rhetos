50d2db3702730d9beb4965cbe224794ea0928a17
Ivan Barbančić <ibarbancic@omega-software.hr>
2021-05-21 11:26:43 +0200

    Generating nuget symbol packages during build

--- CreateInstallationPackage.bat
+++ CreateInstallationPackage.bat
@@ -6,13 +6,21 @@ SETLOCAL
 IF NOT EXIST Install\ MD Install
 DEL /F /S /Q Install\* || GOTO Error0
 
-NuGet.exe pack Source\Rhetos.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack Source\Rhetos.Host.Net.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack Source\Rhetos.Host.AspNet.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack Source\Rhetos.MSBuild.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack Source\Rhetos.Host.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack Source\Rhetos.TestCommon.nuspec -OutputDirectory Install || GOTO Error0
-NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.Net.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.AspNet.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack Source\Rhetos.MSBuild.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack Source\Rhetos.TestCommon.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -OutputDirectory Install -Exclude "**.pdb" || GOTO Error0
+
+NuGet.exe pack Source\Rhetos.nuspec  -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.Net.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.AspNet.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.MSBuild.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.Host.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack Source\Rhetos.TestCommon.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
+NuGet.exe pack CommonConcepts\Rhetos.CommonConcepts.nuspec -Symbols -SymbolPackageFormat snupkg -OutputDirectory Install || GOTO Error0
 
 @REM ================================================
 
