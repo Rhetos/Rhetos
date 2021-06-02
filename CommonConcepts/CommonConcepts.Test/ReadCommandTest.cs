4bc334797aee90d26e8cb7dd58a2dfe6f427f8e6
Bojan Antolović <bojan.antolovic@gmail.com>
2021-06-02 12:36:57 +0200

    Returned unit tests for QueryDataSourceCommand (removed in 1c51ac), but modified to use ReadCommand *directly*.

---
+++ CommonConcepts/CommonConcepts.Test/ReadCommandTest.cs
@@ -0,0 +1,266 @@
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
+
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Features.Indexed;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Processing;
+using Rhetos.Processing.DefaultCommands;
+using Rhetos.XmlSerialization;
+using Rhetos.Configuration.Autofac;
+using Rhetos.Utilities;
+using Rhetos.TestCommon;
+using Rhetos.Logging;
+using Rhetos;
+using CommonConcepts.Test.Helpers;
+
+namespace CommonConcepts.Test
+{
+    [TestClass]
+    public class ReadCommandTest
+    {
+        private static void InitializeData(UnitOfWorkScope scope)
+        {
+            scope.Resolve<ISqlExecuter>().ExecuteSql(new[]
+                {
+                    "DELETE FROM TestQueryDataStructureCommand.E;",
+                    "INSERT INTO TestQueryDataStructureCommand.E(Name) SELECT 'a';",
+                    "INSERT INTO TestQueryDataStructureCommand.E(Name) SELECT 'b';",
+                    "INSERT INTO TestQueryDataStructureCommand.E(Name) SELECT 'c';",
+                    "INSERT INTO TestQueryDataStructureCommand.E(Name) SELECT 'd';",
+                    "INSERT INTO TestQueryDataStructureCommand.E(Name) SELECT 'e';"
+                });
+        }
+
+        private static string ReportCommandResult(UnitOfWorkScope scope, ReadCommandInfo info, bool sort = false)
+        {
+            var commands = scope.Resolve<IIndex<Type, IEnumerable<ICommandImplementation>>>();
+            var readCommand = (ReadCommand)commands[typeof(ReadCommandInfo)].Single();
+
+            var result = (ReadCommandResult)readCommand.Execute(info).Data.Value;
+            var items = ((IEnumerable<TestQueryDataStructureCommand.E>)result.Records).Select(item => item.Name);
+            if (sort)
+                items = items.OrderBy(x => x);
+            var report = string.Join(", ", items) + " /" + result.TotalCount.Value;
+            Console.WriteLine(report);
+            return report;
+        }
+
+        [TestMethod]
+        public void Entity()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo { DataSource = "TestQueryDataStructureCommand.E", ReadRecords = true, ReadTotalCount = true };
+                Assert.AreEqual("a, b, c, d, e /5", ReportCommandResult(scope, info, true));
+            }
+        }
+
+        [TestMethod]
+        public void Ordering()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo
+                {
+                    DataSource = "TestQueryDataStructureCommand.E",
+                    OrderByProperties = new[] { new OrderByProperty { Property = "Name", Descending = true } },
+                    ReadRecords = true,
+                    ReadTotalCount = true
+                };
+                Assert.AreEqual("e, d, c, b, a /5", ReportCommandResult(scope, info));
+            }
+        }
+
+        [TestMethod]
+        public void Paging()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo
+                {
+                    DataSource = "TestQueryDataStructureCommand.E",
+                    Top = 3,
+                    Skip = 3,
+                    OrderByProperties = new[] { new OrderByProperty { Property = "Name", Descending = true } },
+                    ReadRecords = true,
+                    ReadTotalCount = true
+                };
+                Assert.AreEqual("b, a /5", ReportCommandResult(scope, info));
+            }
+        }
+
+        [TestMethod]
+        public void PagingWithoutOrder()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo
+                {
+                    DataSource = "TestQueryDataStructureCommand.E",
+                    Top = 3,
+                    Skip = 3,
+                    ReadRecords = true,
+                    ReadTotalCount = true
+                };
+
+                TestUtility.ShouldFail(() => ReportCommandResult(scope, info), "Sort order must be set if paging is used");
+            }
+        }
+
+        [TestMethod]
+        public void Filters()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo
+                {
+                    DataSource = "TestQueryDataStructureCommand.E",
+                    Filters = new[] { new FilterCriteria { Property = "Name", Operation = "NotEqual", Value = "c" } },
+                    ReadRecords = true,
+                    ReadTotalCount = true
+                };
+                Assert.AreEqual("a, b, d, e /4", ReportCommandResult(scope, info, true));
+            }
+        }
+
+        [TestMethod]
+        public void GenericFilterWithPaging()
+        {
+            using (var scope = TestScope.Create())
+            {
+                InitializeData(scope);
+
+                var info = new ReadCommandInfo
+                {
+                    DataSource = "TestQueryDataStructureCommand.E",
+                    Filters = new[] { new FilterCriteria { Property = "Name", Operation = "NotEqual", Value = "c" } },
+                    Top = 2,
+                    Skip = 2,
+                    OrderByProperties = new[] { new OrderByProperty { Property = "Name" } },
+                    ReadRecords = true,
+                    ReadTotalCount = true
+                };
+                Assert.AreEqual("d, e /4", ReportCommandResult(scope, info));
+            }
+        }
+
+        //====================================================================
+
+        private static string ReportCommandResult2(UnitOfWorkScope scope, ReadCommandInfo info, bool sort = false)
+        {
+            var commands = scope.Resolve<IIndex<Type, IEnumerable<ICommandImplementation>>>();
+            var readCommand = (ReadCommand)commands[typeof(ReadCommandInfo)].Single();
+
+            var result = (ReadCommandResult)readCommand.Execute(info).Data.Value;
+            var items = ((IEnumerable<TestQueryDataStructureCommand.Source>)result.Records).Select(item => item.Name);
+            if (sort)
+                items = items.OrderBy(x => x);
+            var report = string.Join(", ", items) + " /" + result.TotalCount.Value;
+            Console.WriteLine(report);
+            return report;
+        }
+
+
+        [TestMethod]
+        public void Filter()
+        {
+            using (var scope = TestScope.Create())
+            {
+                scope.Resolve<ISqlExecuter>().ExecuteSql(new[] { "DELETE FROM TestQueryDataStructureCommand.Source;" });
+                scope.Resolve<ISqlExecuter>().ExecuteSql(new[] { "a1", "b1", "b2", "c1" }
+                    .Select(name => "INSERT INTO TestQueryDataStructureCommand.Source (Name) SELECT N'" + name + "';"));
+
+                var info = new ReadCommandInfo { DataSource = "TestQueryDataStructureCommand.Source", ReadRecords = true, ReadTotalCount = true };
+                Assert.AreEqual("a1, b1, b2, c1 /4", ReportCommandResult2(scope, info, true));
+
+                info.Filters = new[] { new FilterCriteria(new TestQueryDataStructureCommand.FilterByPrefix { Prefix = "b" }) };
+                Assert.AreEqual("b1, b2 /2", ReportCommandResult2(scope, info, true));
+
+                info.OrderByProperties = new[] { new OrderByProperty { Property = "Name", Descending = true } };
+                Assert.AreEqual("b2, b1 /2", ReportCommandResult2(scope, info));
+
+                info.Top = 1;
+                Assert.AreEqual("b2 /2", ReportCommandResult2(scope, info));
+
+                info.Filters = info.Filters.Concat(new[] { new FilterCriteria { Property = "Name", Operation = "Contains", Value = "1" } }).ToArray();
+                Assert.AreEqual("b1 /1", ReportCommandResult2(scope, info));
+            }
+        }
+
+        [TestMethod]
+        public void NullGenericFilter()
+        {
+            using (var scope = TestScope.Create())
+            {
+                var genericRepos = scope.Resolve<GenericRepositories>().GetGenericRepository("Common.Claim");
+
+                var readCommand = new ReadCommandInfo
+                {
+                    DataSource = "Common.Claim",
+                    Top = 3,
+                    OrderByProperties = new[] { new OrderByProperty { Property = "ClaimResource" } },
+                    ReadRecords = true,
+                    ReadTotalCount = true,
+                };
+
+                var serverCommandsUtility = scope.Resolve<ServerCommandsUtility>();
+
+                var readResult = serverCommandsUtility.ExecuteReadCommand(readCommand, genericRepos);
+                Console.WriteLine("Records.Length: " + readResult.Records.Length);
+                Console.WriteLine("TotalCount: " + readResult.TotalCount);
+                Assert.IsTrue(readResult.Records.Length < readResult.TotalCount);
+            }
+        }
+    }
+
+    //====================================================================
+
+    class SimpleDataTypeProvider : IDataTypeProvider
+    {
+        public IBasicData<T> CreateBasicData<T>(T value)
+        {
+            return new XmlBasicData<T> { Data = value };
+        }
+
+        public IDataArray CreateDataArray<T>(Type type, T[] data) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataArray CreateDomainDataArray(string domainType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CommonConcepts/Plugins/Rhetos.CommonConcepts.Test/ServerCommandsUtilityTest.cs
@@ -0,0 +1,206 @@
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
+
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhetos.CommonConcepts.Test.Mocks;
+using Rhetos.Dom.DefaultConcepts;
+using Rhetos.Processing.DefaultCommands;
+using Rhetos.TestCommon;
+using Rhetos.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rhetos.CommonConcepts.Test
+{
+    static class LegacyExtension
+    {
+        public static ReadCommandResult ExecuteReadCommand(
+            this GenericRepository<IEntity> genericRepository,
+            ReadCommandInfo readCommandInfo)
+        {
+            readCommandInfo.DataSource = typeof(ServerCommandsUtilityTest.SimpleEntity).FullName;
+            var serverCommandsUtility = new ServerCommandsUtility(new ConsoleLogProvider(), new ApplyFiltersOnClientRead());
+            var commandResult = serverCommandsUtility.ExecuteReadCommand(readCommandInfo, genericRepository);
+            return commandResult;
+        }
+    }
+
+    [TestClass]
+    public class ServerCommandsUtilityTest
+    {
+        interface ISimpleEntity : IEntity
+        {
+            string Name { get; set; }
+        }
+
+        public class SimpleEntity : ISimpleEntity
+        {
+            public Guid ID { get; set; }
+            public string Name { get; set; }
+            public string Data { get; set; }
+
+            public override string ToString()
+            {
+                return Name != null ? Name : "<null>";
+            }
+        }
+
+        class SimpleEntityList : List<SimpleEntity>
+        {
+            int idCounter = 0;
+            public void Add(string name) { Add(new SimpleEntity { Name = name, ID = new Guid(idCounter++, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0) }); }
+        }
+
+        GenericRepository<IEntity> NewRepos(IRepository repository)
+        {
+            return new TestGenericRepository<IEntity, SimpleEntity>(repository);
+        }
+
+        class ImplicitReadCommandRepository : IRepository
+        {
+            public int QueryCount = 0;
+            public int DropQueryCount() { int t = QueryCount; QueryCount = 0; return t; }
+
+            public IQueryable<SimpleEntity> Query()
+            {
+                return new[] { 1 }
+                    .SelectMany(x =>
+                    {
+                        QueryCount++;
+                        return new SimpleEntityList { "a1", "b1", "b2", "b3", "b4", "b5" };
+                    }).AsQueryable();
+            }
+
+            public IEnumerable<SimpleEntity> Filter(IEnumerable<SimpleEntity> source, string pattern)
+            {
+                return source.Where(item => item.Name.Contains(pattern));
+            }
+
+            public static readonly KeyValuePair<string, Type>[] ReadParameterTypes = new KeyValuePair<string, Type>[]
+            {};
+        }
+
+        class ExplicitReadCommandRepository : IRepository
+        {
+            public ReadCommandResult ReadCommand(ReadCommandInfo commandInfo)
+            {
+                return new ReadCommandResult
+                {
+                    Records = new SimpleEntityList { "a1", "b1", "b2" }.ToArray(),
+                    TotalCount = 10
+                };
+            }
+        }
+
+        static string Dump(ReadCommandResult commandResult)
+        {
+            return TestUtility.Dump(commandResult.Records) + " / " + commandResult.TotalCount.Value;
+        }
+
+        [TestMethod]
+        public void ReadCommand()
+        {
+            var entityRepos = new ImplicitReadCommandRepository();
+            var genericRepos = NewRepos(entityRepos);
+
+            var command = new ReadCommandInfo
+            {
+                Filters = new[] { new FilterCriteria { Property = "Name", Operation = "StartsWith", Value = "b" } },
+                OrderByProperties = new[] { new OrderByProperty { Property = "Name" } },
+                Top = 3,
+                Skip = 3,
+                ReadRecords = true,
+                ReadTotalCount = true,
+            };
+            Assert.AreEqual("a1, b1, b2 / 10", Dump(NewRepos(new ExplicitReadCommandRepository()).ExecuteReadCommand(command)));
+            Assert.AreEqual(0, entityRepos.DropQueryCount());
+            Assert.AreEqual("b4, b5 / 5", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(2, entityRepos.DropQueryCount()); // Paging should result with two queries: selecting items and count.
+
+            command = new ReadCommandInfo { ReadRecords = true, ReadTotalCount = true };
+            Assert.AreEqual("a1, b1, b2, b3, b4, b5 / 6", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(1, entityRepos.DropQueryCount()); // Without paging, there is no need for two queries.
+
+            command = new ReadCommandInfo { Filters = new[] { new FilterCriteria("1") }, ReadRecords = true, ReadTotalCount = true };
+            Assert.AreEqual("a1, b1 / 2", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(1, entityRepos.DropQueryCount()); // Without paging, there is no need for two queries.
+
+            command = new ReadCommandInfo { Filters = new[] { new FilterCriteria("1"), new FilterCriteria { Property = "Name", Operation = "StartsWith", Value = "b" } }, ReadRecords = true, ReadTotalCount = true };
+            Assert.AreEqual("b1 / 1", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(1, entityRepos.DropQueryCount()); // Without paging, there is no need for two queries.
+
+            command = new ReadCommandInfo { Filters = new[] { new FilterCriteria("1"), new FilterCriteria { Filter = "System.String", Value = "b" } }, ReadRecords = true, ReadTotalCount = true };
+            Assert.AreEqual("b1 / 1", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(1, entityRepos.DropQueryCount()); // Without paging, there is no need for two queries.
+
+            command = new ReadCommandInfo { Filters = new[] { new FilterCriteria("b") }, Top = 2, Skip = 2, OrderByProperties = new[] { new OrderByProperty { Property = "Name" } }, ReadRecords = true, ReadTotalCount = true };
+            Assert.AreEqual("b3, b4 / 5", Dump(genericRepos.ExecuteReadCommand(command)));
+            Assert.AreEqual(1, entityRepos.DropQueryCount()); // Enumerable filter will cause GenericRepository to materialize of the query, so it will be executed only once even though the paging is used.
+        }
+
+        [TestMethod]
+        public void ReadCommand_OutsideInterface()
+        {
+            var entityRepos = new ImplicitReadCommandRepository();
+            var genericRepos = NewRepos(entityRepos);
+
+            var command = new ReadCommandInfo
+            {
+                Filters = new[] { new FilterCriteria { Property = "Name", Operation = "StartsWith", Value = "b" } },
+                OrderByProperties = new[] { new OrderByProperty { Property = "Data" } },
+                Top = 3,
+                Skip = 3,
+                ReadRecords = true,
+                ReadTotalCount = true,
+            };
+            Assert.AreEqual("b4, b5 / 5", Dump(genericRepos.ExecuteReadCommand(command)));
+
+            command = new ReadCommandInfo
+            {
+                Filters = new[] { new FilterCriteria { Property = "Data", Operation = "Equal", Value = "xxx" } },
+                OrderByProperties = new[] { new OrderByProperty { Property = "Name" } },
+                Top = 3,
+                Skip = 3,
+                ReadRecords = true,
+                ReadTotalCount = true,
+            };
+            Assert.AreEqual(" / 0", Dump(genericRepos.ExecuteReadCommand(command)));
+        }
+
+        [TestMethod]
+        public void ReadCommand_Null()
+        {
+            var entityRepos = new ImplicitReadCommandRepository();
+            var genericRepos = NewRepos(entityRepos);
+
+            var command = new ReadCommandInfo
+            {
+                Filters = new[] { new FilterCriteria { Property = "Data", Operation = "Equal", Value = null } },
+                OrderByProperties = new[] { new OrderByProperty { Property = "Name" } },
+                Top = 3,
+                Skip = 3,
+                ReadRecords = true,
+                ReadTotalCount = true,
+            };
+            Assert.AreEqual("b3, b4, b5 / 6", Dump(genericRepos.ExecuteReadCommand(command)));
+        }
+    }
+}
