8e6244fce7699488ca636bd8b1de3810cf0b8c8c
Bojan Antolović <bojan.antolovic@gmail.com>
2023-10-24 16:12:29 +0200

    The following libraries are replaced with Rhetos.Core.dll: Rhetos.Compiler.dll, Rhetos.Compiler.Interfaces.dll, Rhetos.DatabaseGenerator.dll, Rhetos.DatabaseGenerator.Interfaces.dll, Rhetos.Dom.dll, Rhetos.Dom.Interfaces.dll, Rhetos.Dsl.dll, Rhetos.Extensibility.dll, Rhetos.Extensibility.Interfaces.dll, Rhetos.Logging.dll, Rhetos.Persistence.dll, Rhetos.Persistence.Interfaces.dll, Rhetos.Processing.dll, Rhetos.Processing.Interfaces.dll, Rhetos.Security.dll, Rhetos.Security.Interfaces.dll, Rhetos.Utilities.dll, Rhetos.Configuration.Autofac.dll, Rhetos.Deployment.dll. (breaking change) Namespaces have not been changed.

---
+++ src/Rhetos.Core/Extensibility/PluginScannerOptions.cs
@@ -0,0 +1,58 @@
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
+
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System.Collections.Generic;
+
+namespace Rhetos.Extensibility
+{
+    /// <summary>
+    /// Build-time configuration.
+    /// </summary>
+    [Options("Rhetos:PluginScanner")]
+    public class PluginScannerOptions
+    {
+        /// <summary>
+        /// List of file names or file name prefixes that will be ignored when scanning for plugins.
+        /// If an entry ends with '*', it will be considered a file name prefix, otherwise an exact match will be used.
+        /// Names are case-insensitive.
+        /// These entries will be appended to <see cref="PredefinedIgnoreAssemblyFiles"/>.
+        /// </summary>
+        public IEnumerable<string> IgnoreAssemblyFiles { get; set; }
+
+        public IEnumerable<string> PredefinedIgnoreAssemblyFiles { get; set; } = _predefinedIgnoreAssemblyFiles;
+
+        private static readonly IEnumerable<string> _predefinedIgnoreAssemblyFiles = new[]
+        {
+            "Autofac.*",
+            "EntityFramework.*",
+            "Microsoft.*",
+            "NuGet.*",
+            "System.*",
+            "Newtonsoft.Json.dll",
+            "NLog.dll",
+            "Oracle.ManagedDataAccess.dll",
+            "rhetos.dll",
+            "Rhetos.Core.dll",
+            "Rhetos.DslParser.dll",
+            "Rhetos.Integration.dll",
+            "Rhetos.TestCommon.dll",
+            "RhetosVSIntegration.dll",
+        };
+    }
+}
---
+++ src/Rhetos.Core/Utilities/Legacy/LegacyUtilities.cs
@@ -0,0 +1,39 @@
+﻿/*
+    Copyright (C) 2014 Omega software d.o.o.
+
+    This file is part of Rhetos.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Rhetos.Utilities;
+
+namespace Rhetos
+{
+    public static class LegacyUtilities
+    {
+        /// <summary>
+        /// Use to initialize obsolete static utilities <see cref="ConfigUtility"/>, <see cref="Utilities.Configuration"/> and <see cref="SqlUtility"/> 
+        /// prior to using any of their methods. This will bind those utilities to configuration source compliant with new configuration convention.
+        /// </summary>
+        public static void Initialize(IConfiguration configuration)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            ConfigUtility.Initialize(configuration);
+            SqlUtility.Initialize(configuration);
+            Utilities.Configuration.Initialize(configuration);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
