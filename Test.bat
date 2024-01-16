7fd9741db60801aebb0489db8b2a4945dd62fc48
Bojan Antolović <bojan.antolovic@gmail.com>
2024-01-16 11:48:40 +0100

    Corrected error handling in Test.bat.

--- Test.bat
+++ Test.bat
@@ -10,7 +10,7 @@ IF NOT EXIST test\CommonConcepts.TestApp\local.settings.json ECHO Missing local.
 @REM Using "RestoreForce" to make sure that the new version of local Rhetos NuGet packages are included in build.
 dotnet build CommonConceptsTest.sln /t:restore /p:RestoreForce=True /t:rebuild --configuration %Config% || GOTO Error0
 @REM Running dbupdate again to test the CLI (it was executed in the build above).
-dotnet test\CommonConcepts.TestApp\bin\Debug\net8.0\rhetos.dll dbupdate test\CommonConcepts.TestApp\bin\Debug\net8.0\CommonConcepts.TestApp.dll
+dotnet test\CommonConcepts.TestApp\bin\Debug\net8.0\rhetos.dll dbupdate test\CommonConcepts.TestApp\bin\Debug\net8.0\CommonConcepts.TestApp.dll || GOTO Error0
 dotnet test CommonConceptsTest.sln --no-build || GOTO Error0
 
 IF EXIST "%ProgramFiles%\LINQPad8\LPRun8.exe" "%ProgramFiles%\LINQPad8\LPRun8.exe" "test\CommonConcepts.TestApp\bin\Debug\net8.0\LinqPad\Rhetos DOM.linq" > nul || GOTO Error0
