1bdac6664d0ba29d5736565cfbf83a650c63b62e
Bojan Antolović <bojan.antolovic@gmail.com>
2024-02-06 10:25:01 +0100

    Test.bat executes test on both Rhetos.MsSql and Rhetos.MsSqlEf6.

--- Test.bat
+++ Test.bat
@@ -7,13 +7,9 @@ SETLOCAL
 dotnet test Rhetos.sln --no-build || GOTO Error0
 
 IF NOT EXIST test\CommonConcepts.TestApp\local.settings.json ECHO Missing local.settings.json. Follow the Initial setup instructions in Readme.md. & GOTO Error0
-@REM Using "RestoreForce" to make sure that the new version of local Rhetos NuGet packages are included in build.
-dotnet build CommonConceptsTest.sln /t:restore /p:RestoreForce=True /t:rebuild --configuration %Config% || GOTO Error0
-@REM Running dbupdate again to test the CLI (it was executed in the build above).
-dotnet test\CommonConcepts.TestApp\bin\Debug\net8.0\rhetos.dll dbupdate test\CommonConcepts.TestApp\bin\Debug\net8.0\CommonConcepts.TestApp.dll || GOTO Error0
-dotnet test CommonConceptsTest.sln --no-build || GOTO Error0
 
-IF EXIST "%ProgramFiles%\LINQPad8\LPRun8.exe" "%ProgramFiles%\LINQPad8\LPRun8.exe" "test\CommonConcepts.TestApp\bin\Debug\net8.0\LinqPad\Rhetos DOM.linq" > nul || GOTO Error0
+CALL :TestAppWithDatabase Rhetos.MsSql || GOTO Error0
+CALL :TestAppWithDatabase Rhetos.MsSqlEf6 || GOTO Error0
 
 @REM ================================================
 
@@ -24,4 +20,26 @@ IF EXIST "%ProgramFiles%\LINQPad8\LPRun8.exe" "%ProgramFiles%\LINQPad8\LPRun8.ex
 :Error0
 @ECHO.
 @ECHO %~nx0 FAILED.
+@EXIT /B 1
+
+@REM ================================================
+:TestAppWithDatabase
+
+dotnet remove test\CommonConcepts.TestApp package Rhetos.MsSql > nul
+dotnet remove test\CommonConcepts.TestApp package Rhetos.MsSqlEf6 > nul
+dotnet add test\CommonConcepts.TestApp package %1 --version 6.0.0-dev* || GOTO DatabaseError0
+
+@REM Using "RestoreForce" to make sure that the new version of local Rhetos NuGet packages are included in build.
+dotnet build CommonConceptsTest.sln /t:restore /p:RestoreForce=True /t:rebuild --configuration %Config% || GOTO DatabaseError0
+@REM Running dbupdate again to test the CLI (it was executed in the build above).
+dotnet test\CommonConcepts.TestApp\bin\Debug\net8.0\rhetos.dll dbupdate test\CommonConcepts.TestApp\bin\Debug\net8.0\CommonConcepts.TestApp.dll || GOTO DatabaseError0
+dotnet test CommonConceptsTest.sln --no-build || GOTO DatabaseError0
+
+IF EXIST "%ProgramFiles%\LINQPad8\LPRun8.exe" "%ProgramFiles%\LINQPad8\LPRun8.exe" "test\CommonConcepts.TestApp\bin\Debug\net8.0\LinqPad\Rhetos DOM.linq" > nul || GOTO DatabaseError0
+
+@EXIT /B 0
+
+:DatabaseError0
+@ECHO.
+@ECHO Error while testing with '%1'.
 @EXIT /B 1
