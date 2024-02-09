0e00468cbfb3767dea2ca01ebdcca89c3b9cf9ff
Bojan Antolović <bojan.antolovic@gmail.com>
2024-02-09 10:58:26 +0100

    TestAppDirectMsSqlEfCore test project.

---
+++ test/TestAppDirectMsSqlEfCore/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Rhetos.Security;
+using Rhetos.Utilities;
+using Rhetos;
+using Autofac;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args);
+builder.ConfigureServices(services =>
+{
+    services.AddRhetosHost((IServiceProvider serviceProvider, IRhetosHostBuilder rhetosHostBuilder) =>
+        {
+            rhetosHostBuilder
+                .ConfigureRhetosAppDefaults()
+                .ConfigureConfiguration(configurationBuilder => configurationBuilder
+                    .AddJsonFile("local.settings.json"))
+                .ConfigureContainer(builder =>
+                {
+                    builder.RegisterType<ProcessUserInfo>().As<IUserInfo>();
+                });
+        });
+});
+
+IHost host = builder.Build();
+host.Run();
