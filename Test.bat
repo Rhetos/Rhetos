6b5225c165c3dfdf83947f9b1214551e76bb8122
Bojan Antolović <bojan.antolovic@gmail.com>
2024-03-07 11:02:25 +0100

    Separated TestAppWithDatabase.bat from Test.bat, to allow direct usage.

--- Test.bat
+++ Test.bat
@@ -8,8 +8,8 @@ dotnet test Rhetos.sln --no-build || GOTO Error0
 
 IF NOT EXIST test\CommonConcepts.TestApp\local.settings.json ECHO Missing local.settings.json. Follow the Initial setup instructions in Readme.md. & GOTO Error0
 
-CALL :TestAppWithDatabase Rhetos.MsSql || GOTO Error0
-CALL :TestAppWithDatabase Rhetos.MsSqlEf6 || GOTO Error0
+CALL tools\Build\TestAppWithDatabase.bat Rhetos.MsSql %Config% || GOTO Error0
+CALL tools\Build\TestAppWithDatabase.bat Rhetos.MsSqlEf6 %Config% || GOTO Error0
 
 @REM ================================================
 
@@ -20,32 +20,4 @@ CALL :TestAppWithDatabase Rhetos.MsSqlEf6 || GOTO Error0
 :Error0
 @ECHO.
 @ECHO %~nx0 FAILED.
-@EXIT /B 1
-
-@REM ================================================
-:TestAppWithDatabase
-
-TITLE Testing %1
-
-IF %1 NEQ Rhetos.MsSqlEf6 dotnet remove test\CommonConcepts.TestApp package Rhetos.MsSqlEf6 > nul
-IF %1 NEQ Rhetos.MsSql dotnet remove test\CommonConcepts.TestApp package Microsoft.EntityFrameworkCore.Proxies > nul
-IF %1 NEQ Rhetos.MsSql dotnet remove test\CommonConcepts.TestApp package Rhetos.MsSql > nul
-
-IF %1 EQU Rhetos.MsSqlEf6 dotnet add test\CommonConcepts.TestApp package Rhetos.MsSqlEf6 --version 6.0.0-dev* || GOTO DatabaseError0
-IF %1 EQU Rhetos.MsSql dotnet add test\CommonConcepts.TestApp package Rhetos.MsSql --version 6.0.0-dev* || GOTO DatabaseError0
-IF %1 EQU Rhetos.MsSql dotnet add test\CommonConcepts.TestApp package Microsoft.EntityFrameworkCore.Proxies --version 8.0.* || GOTO DatabaseError0
-
-@REM Using "RestoreForce" to make sure that the new version of local Rhetos NuGet packages are included in build.
-dotnet build CommonConceptsTest.sln /t:restore /p:RestoreForce=True /t:rebuild --configuration %Config% || GOTO DatabaseError0
-@REM Running dbupdate again to test the CLI (it was executed in the build above).
-dotnet test\CommonConcepts.TestApp\bin\Debug\net8.0\rhetos.dll dbupdate test\CommonConcepts.TestApp\bin\Debug\net8.0\CommonConcepts.TestApp.dll || GOTO DatabaseError0
-dotnet test CommonConceptsTest.sln --no-build || GOTO DatabaseError0
-
-IF EXIST "%ProgramFiles%\LINQPad8\LPRun8.exe" "%ProgramFiles%\LINQPad8\LPRun8.exe" "test\CommonConcepts.TestApp\bin\Debug\net8.0\LinqPad\Rhetos DOM.linq" > nul || GOTO DatabaseError0
-
-@EXIT /B 0
-
-:DatabaseError0
-@ECHO.
-@ECHO Error while testing with '%1'.
 @EXIT /B 1
