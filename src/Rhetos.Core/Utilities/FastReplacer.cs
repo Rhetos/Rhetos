8137fb5da02dfd8c6ad684f755c67bc6eb47987a
jshergal <1489638+jshergal@users.noreply.github.com>
2025-06-24 12:29:33 +0300

    Updated FastReplacer and FastReplacerSnippet to CPOL 1.02

--- src/Rhetos.Core/Utilities/FastReplacer.cs
+++ src/Rhetos.Core/Utilities/FastReplacer.cs
@@ -1,20 +1,12 @@
 ﻿/*
-    Copyright (C) 2014 Omega software d.o.o.
+    This code is licensed under the Code Project Open License (CPOL) 1.02.
+    
+    CPOL 1.02 - https://www.codeproject.com/info/cpol10.aspx
+    
+    Originally published at:
+    https://www.codeproject.com/Articles/298519/Fast-Token-Replacement-in-Csharp
 
-    This file is part of Rhetos.
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    By Bojan Antolovic
 */
 
 using System;
